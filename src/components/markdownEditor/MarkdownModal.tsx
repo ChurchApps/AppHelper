af8ef977be2ce46aef6d545eb4963f7e9058caeb
Jeremy Zongker <jeremy@zongker.net>
2024-06-21 17:04:55 -0500

    Added translations for popular languages.

--- src/components/markdownEditor/MarkdownModal.tsx
+++ src/components/markdownEditor/MarkdownModal.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, TextField } from "@mui/material";
 import { MarkdownPreview } from "./MarkdownPreview";
-import { Locale } from "../helpers";
+import { Locale } from "../../helpers";
 
 interface Props {
   hideModal: () => void
