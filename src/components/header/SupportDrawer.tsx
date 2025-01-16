607407d94d0932e804ee728edaaf20bea2e60c99
Himali Malvawala <himalimalvawala@gmail.com>
2025-01-16 12:44:08 +0530

    chore: remove extra lines

--- src/components/header/SupportDrawer.tsx
+++ src/components/header/SupportDrawer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Box, Button, Drawer, Icon, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, Tooltip, Typography } from "@mui/material";
+import { Avatar, Box, Button, Drawer, Icon, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import { ApiHelper, GenericSettingInterface } from "../../helpers";
 
 type Props = {
@@ -15,6 +15,7 @@ export const SupportDrawer = (props: Props) => {
 
   let currentAppName = "";
   if (props.appName === "CHUMS") currentAppName = "chums";
+	if (props.appName === "B1") currentAppName = "b1";
 
   const validateEmail = (email: string) => {
     return email.match(
@@ -31,120 +32,47 @@ export const SupportDrawer = (props: Props) => {
   };
 
   const loadData = () => {
-    ApiHelper.get("/settings", "MembershipApi").then(
-      (data: GenericSettingInterface[]) => {
-        const contactRes = data.filter((d) => d.keyName === "supportContact");
-        if (contactRes?.length > 0 && contactRes[0].value !== "")
-          setSupportContact(contactRes[0]);
+    ApiHelper.get("/settings", "MembershipApi").then((data: GenericSettingInterface[]) => {
+			const contactRes = data.filter((d) => d.keyName === "supportContact");
+			if (contactRes?.length > 0 && contactRes[0].value !== "") setSupportContact(contactRes[0]);
 
-        const logoRes = data.filter((d) => d.keyName === "favicon_16x16");
-        if (logoRes?.length > 0 && logoRes[0].value !== "")
-          setChurchLogo(logoRes[0]);
-      }
-    );
+			const logoRes = data.filter((d) => d.keyName === "favicon_16x16");
+			if (logoRes?.length > 0 && logoRes[0].value !== "") setChurchLogo(logoRes[0]);
+		});
   };
 
   useEffect(loadData, []);
 
   return (
     <>
-      <IconButton
-        sx={{ color: "white !important", borderRadius: 2 }}
-        onClick={() => setOpen(true)}
-      >
+      <IconButton sx={{ color: "white !important", borderRadius: 2 }} onClick={() => setOpen(true)}>
         <Icon sx={{ fontSize: "26px !important" }}>help</Icon>
       </IconButton>
-      <Drawer
-        open={open}
-        onClose={() => setOpen(false)}
-        anchor="right"
-        PaperProps={{
-          sx: { width: { xs: "80%", sm: "40%", md: "30%", lg: "20%" } },
-        }}
-      >
+      <Drawer open={open} onClose={() => setOpen(false)} anchor="right" PaperProps={{ sx: { width: { xs: "80%", sm: "40%", md: "30%", lg: "20%" } } }}>
         <Box
-          sx={{
-            paddingTop: 10,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            minHeight: "98vh",
-          }}
-        >
+          sx={{ paddingTop: 10, display: "flex", flexDirection: "column", justifyContent: "space-between", minHeight: "98vh" }}>
           <Box>
-            {/* <Typography variant="h6" sx={{ fontWeight: "bold", color: "#568bda" }}>Help</Typography> */}
             {supportContact ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "end",
-                  alignItems: "center",
-                }}
-              >
-                {/* <div></div> */}
-                {/* //if mail then open mail or open in new window */}
-                <Box
-                  sx={{
-                    cursor: "pointer",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginRight: 2,
-                    borderRadius: 2,
-                    padding: "7px",
-                    backgroundColor: "#e9e9e9",
-                  }}
-                  component="div"
-                  onClick={() => handleChurchSupportClick()}
-                >
-                  <Avatar
-                    variant="rounded"
-                    src={churchLogo ? churchLogo.value : null}
-                    sx={{
-                      marginRight: 1,
-                      bgcolor: "#568bda",
-                      width: 25,
-                      height: 25,
-                    }}
-                  >
+              <Box sx={{ display: "flex", justifyContent: "end", alignItems: "center" }}>
+                <Box sx={{ cursor: "pointer", display: "flex", justifyContent: "center", alignItems: "center", marginRight: 2, borderRadius: 2, padding: "7px", backgroundColor: "#e9e9e9" }} component="div" onClick={() => handleChurchSupportClick()}>
+                  <Avatar variant="rounded" src={churchLogo ? churchLogo.value : null} sx={{ marginRight: 1, bgcolor: "#568bda", width: 25, height: 25 }}>
                     <Icon fontSize="small">church</Icon>
                   </Avatar>
-                  <Typography sx={{ color: "#568bda", fontSize: "13px" }}>
-                    Support
-                  </Typography>
+                  <Typography sx={{ color: "#568bda", fontSize: "13px" }}>Support</Typography>
                 </Box>
-                {/* <Button variant="contained" startIcon={<ChurchIcon />} size="small" sx={{ textAlign: "right" }}>Support</Button> */}
               </Box>
             ) : null}
             <List>
               <ListItem disablePadding>
-                <ListItemButton
-                  href={supportHref + currentAppName}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItemIcon sx={{ minWidth: "38px" }}>
-                    <Icon sx={{ color: "#568bda" }}>article</Icon>
-                  </ListItemIcon>
-                  <ListItemText sx={{ color: "#568bda" }}>
-                    View Documentation
-                  </ListItemText>
+                <ListItemButton href={supportHref + currentAppName} target="_blank" rel="noopener noreferrer">
+                  <ListItemIcon sx={{ minWidth: "38px" }}><Icon sx={{ color: "#568bda" }}>article</Icon></ListItemIcon>
+                  <ListItemText sx={{ color: "#568bda" }}>View Documentation</ListItemText>
                 </ListItemButton>
               </ListItem>
             </List>
             {props?.relatedArticles?.length > 0 ? (
               <Box sx={{ marginTop: 2 }}>
-                <Typography
-                  sx={{
-                    color: "#568bda",
-                    textDecoration: "underline",
-                    textUnderlineOffset: 10,
-                    textDecorationThickness: 2,
-                    marginLeft: 2,
-                  }}
-                >
-                  Features Tour
-                </Typography>
+                <Typography sx={{ color: "#568bda", textDecoration: "underline", textUnderlineOffset: 10, textDecorationThickness: 2, marginLeft: 2 }}>Features Tour</Typography>
                 <List sx={{ marginTop: 1.5 }}>
                   {props.relatedArticles.map((a) => {
                     const parts = a.split("/");
@@ -152,25 +80,9 @@ export const SupportDrawer = (props: Props) => {
                     const result = lastPart.replace("-", " ");
                     return (
                       <ListItem disablePadding>
-                        <ListItemButton
-                          href={supportHref + a}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <ListItemIcon sx={{ minWidth: "35px" }}>
-                            <Icon sx={{ color: "#568bda" }}>play_circle</Icon>
-                          </ListItemIcon>
-                          <ListItemText>
-                            <Typography
-                              sx={{
-                                color: "#568bda",
-                                textTransform: "capitalize",
-                              }}
-                              fontSize="15px"
-                            >
-                              {result}
-                            </Typography>
-                          </ListItemText>
+                        <ListItemButton href={supportHref + a} target="_blank" rel="noopener noreferrer">
+                          <ListItemIcon sx={{ minWidth: "35px" }}><Icon sx={{ color: "#568bda" }}>play_circle</Icon></ListItemIcon>
+                          <ListItemText><Typography sx={{ color: "#568bda", textTransform: "capitalize" }} fontSize="15px">{result}</Typography></ListItemText>
                         </ListItemButton>
                       </ListItem>
                     );
@@ -180,69 +92,14 @@ export const SupportDrawer = (props: Props) => {
             ) : null}
           </Box>
           <Box sx={{ marginRight: 4, marginLeft: 4, marginTop: "auto" }}>
-            <hr
-              style={{
-                color: "#568bda",
-                borderColor: "#568bda",
-                marginLeft: -13,
-                marginRight: -13,
-              }}
-            />
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                color: "#568bda",
-                marginBottom: 1.5,
-                marginTop: 1.5,
-              }}
-            >
-              <Avatar
-                src="https://avatars.githubusercontent.com/u/74469593?s=200&v=4"
-                variant="rounded"
-                sx={{ width: 25, height: 25, marginRight: 1 }}
-              >
-                C
-              </Avatar>
+            <hr style={{ color: "#568bda", borderColor: "#568bda", marginLeft: -13, marginRight: -13 }} />
+            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", color: "#568bda", marginBottom: 1.5, marginTop: 1.5 }}>
+              <Avatar src="https://avatars.githubusercontent.com/u/74469593?s=200&v=4" variant="rounded" sx={{ width: 25, height: 25, marginRight: 1 }}>C</Avatar>
               <Typography>ChurchApps</Typography>
             </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                gap: 4,
-                marginBottom: 1,
-              }}
-            >
-              <Button
-                variant="contained"
-                size="small"
-                startIcon={<Icon fontSize="small">mail</Icon>}
-                fullWidth
-                sx={{ backgroundColor: "#568bda" }}
-              >
-                Support
-              </Button>
-              <Button
-                variant="contained"
-                size="small"
-                startIcon={<Icon fontSize="small">forum</Icon>}
-                fullWidth
-                sx={{ backgroundColor: "#568bda" }}
-                href="https://github.com/orgs/ChurchApps/discussions"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Forum
-              </Button>
-              {/* <Tooltip arrow title="Email Support">
-                        <IconButton><MailIcon sx={{ color: "#568bda" }} /></IconButton>
-                    </Tooltip>
-                    <Tooltip arrow title="Support Forum">
-                        <IconButton><ForumIcon sx={{ color: "#568bda" }} /></IconButton>
-                    </Tooltip> */}
+            <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", gap: 4, marginBottom: 1 }}>
+              <Button variant="contained" size="small" startIcon={<Icon fontSize="small">mail</Icon>} fullWidth sx={{ backgroundColor: "#568bda" }}>Support</Button>
+              <Button variant="contained" size="small" startIcon={<Icon fontSize="small">forum</Icon>} fullWidth sx={{ backgroundColor: "#568bda" }} href="https://github.com/orgs/ChurchApps/discussions" target="_blank" rel="noopener noreferrer">Forum</Button>
             </Box>
           </Box>
         </Box>
