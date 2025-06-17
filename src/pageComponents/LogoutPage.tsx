013032f0afffcf6684c91ca36311506feaf7f3f6
Jeremy Zongker <jeremy@zongker.net>
2025-06-17 01:34:51 -0500

    Fixed redirect issue on login

--- src/pageComponents/LogoutPage.tsx
+++ src/pageComponents/LogoutPage.tsx
@@ -4,27 +4,32 @@ import React from "react";
 import { useCookies } from "react-cookie"
 import { ApiHelper, UserContextInterface } from "../helpers";
 
-interface Props { context?: UserContextInterface, }
+interface Props { context?: UserContextInterface, handleRedirect?: (url: string) => void }
 
 export const LogoutPage: React.FC<Props> = (props) => {
-  const [, , removeCookie] = useCookies(["jwt", "email", "name", "lastChurchId"]);
+	const [, , removeCookie] = useCookies(["jwt", "email", "name", "lastChurchId"]);
 
-  removeCookie("jwt");
-  removeCookie("email");
-  removeCookie("name");
-  removeCookie("lastChurchId");
+	removeCookie("jwt");
+	removeCookie("email");
+	removeCookie("name");
+	removeCookie("lastChurchId");
 
-  ApiHelper.clearPermissions();
-  props.context?.setUser(null);
-  props.context?.setPerson(null);
-  props.context?.setUserChurches(null);
-  props.context?.setUserChurch(null);
+	ApiHelper.clearPermissions();
+	props.context?.setUser(null);
+	props.context?.setPerson(null);
+	props.context?.setUserChurches(null);
+	props.context?.setUserChurch(null);
 
-  setTimeout(() => {
-    // a must check for Nextjs
-    if (typeof window !== "undefined") {
-      window.location.href = "/";
-    }
-  }, 300);
-  return null;
+	setTimeout(() => {
+		// a must check for Nextjs
+		if (typeof window !== "undefined") {
+			// Use handleRedirect function if available, otherwise fallback to window.location
+			if (props.handleRedirect) {
+				props.handleRedirect("/");
+			} else {
+				window.location.href = "/";
+			}
+		}
+	}, 300);
+	return null;
 }
