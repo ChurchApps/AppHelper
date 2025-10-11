68c9927e740e85f69606b6536fd96f4934a01b97
Jeremy Zongker <jeremy@zongker.net>
2025-10-10 20:47:25 -0500

    Fixed loading indicator

--- packages/apphelper/src/components/Loading.tsx
+++ packages/apphelper/src/components/Loading.tsx
@@ -2,53 +2,7 @@
 
 import React from "react";
 import { Dots } from "react-activity";
-
-// Inline CSS for react-activity to avoid external dependencies
-const activityStyles = `
-  .activity-loader {
-    position: relative;
-    display: inline-block;
-  }
-  .activity-dots {
-    position: relative;
-    display: inline-block;
-  }
-  .activity-dots > div {
-    position: absolute;
-    border-radius: 50%;
-    animation: activity-dots 1.2s infinite ease-in-out;
-    animation-fill-mode: both;
-  }
-  .activity-dots > div:nth-child(1) { animation-delay: -1.1s; }
-  .activity-dots > div:nth-child(2) { animation-delay: -1.0s; }
-  .activity-dots > div:nth-child(3) { animation-delay: -0.9s; }
-  .activity-dots > div:nth-child(4) { animation-delay: -0.8s; }
-  .activity-dots > div:nth-child(5) { animation-delay: -0.7s; }
-  .activity-dots > div:nth-child(6) { animation-delay: -0.6s; }
-  .activity-dots > div:nth-child(7) { animation-delay: -0.5s; }
-  .activity-dots > div:nth-child(8) { animation-delay: -0.4s; }
-  .activity-dots > div:nth-child(9) { animation-delay: -0.3s; }
-  .activity-dots > div:nth-child(10) { animation-delay: -0.2s; }
-  .activity-dots > div:nth-child(11) { animation-delay: -0.1s; }
-  .activity-dots > div:nth-child(12) { animation-delay: 0s; }
-
-  @keyframes activity-dots {
-    0%, 80%, 100% {
-      transform: scale(0);
-    }
-    40% {
-      transform: scale(1);
-    }
-  }
-`;
-
-// Add styles to document head if not already present
-if (typeof document !== 'undefined' && !document.getElementById('activity-styles')) {
-  const style = document.createElement('style');
-  style.id = 'activity-styles';
-  style.textContent = activityStyles;
-  document.head.appendChild(style);
-}
+import "react-activity/dist/Dots.css";
 
 interface Props { size?: "sm" | "md" | "lg", loadingText?: string, color?: string }
 
