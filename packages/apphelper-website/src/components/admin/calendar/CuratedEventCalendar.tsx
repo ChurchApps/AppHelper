b811fb29e2f06ce0ded349f4cd03009f0dff8165
Jeremy Zongker <jeremy@zongker.net>
2025-11-06 20:15:36 -0600

    Added recuring event exceptions

--- packages/apphelper-website/src/components/admin/calendar/CuratedEventCalendar.tsx
+++ packages/apphelper-website/src/components/admin/calendar/CuratedEventCalendar.tsx
@@ -93,6 +93,7 @@ export function CuratedEventCalendar(props: Props) {
         evt.end = new Date(date.getTime() + diff);
         expandedEvents.push(evt);
       });
+      EventHelper.removeExcludeDates(expandedEvents);
     } else expandedEvents.push(ev);
   });
 
